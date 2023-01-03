935d9fc881e4aa8f7354bf87a2a65c01da135661
ret7020 <ret7020@gmail.com>
2023-01-03 23:50:57 +0300

    Generator from transforers

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from config import API_TOKEN
 from aiogram import Bot, Dispatcher, executor, types
-
-
+import random
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+import transformers
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+
+print("[DEBUG] Loading model")
+pipe = StableDiffusionPipeline.from_pretrained(
+    'hakurei/waifu-diffusion',
+    torch_dtype=torch.float32
+).to('cuda')
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -21,8 +30,22 @@ async def waifu(message: types.Message):
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    with open('./images/0.png', 'rb') as photo:
-        await message.reply_photo(photo, caption='Waifu')
+  prompt = "looking at viewer, masterpiece, best quality, bright hair, blue eyes, medium boobs in mini skirt, indoors, popular, beautiful"
+
+  # Bypass NSFW checker :)
+  # Only for work; freelance yepta
+
+  def dummy(images, **kwargs):
+      return images, False
+  pipe.safety_checker = dummy
+
+  with autocast("cuda"):
+      image = pipe(prompt, guidance_scale=6).images[0]
+  file_id = random.randint(10000, 10000000)
+  image.save(f"./tmp_gen/{file_id}.png")
+
+  with open(f'./tmp_gen/{file_id}.png', 'rb') as photo:
+      await message.reply_photo(photo, caption='Generated waifu image')
 
 
 if __name__ == '__main__':
